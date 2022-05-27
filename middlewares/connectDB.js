46017eefb5cc1f4eac98cd7daeeaabb0466cf8ab
KDA <iamkda@yandex.ru>
2022-05-27 22:37:12 +0300

    fix: fix DB_ADDRESS into connectDB

--- middlewares/connectDB.js
+++ middlewares/connectDB.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+const { NODE_ENV, DB_ADDRESS } = process.env;
+
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.DB_ADDRESS);
+    await mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://localhost:27017/moviesdb');
     console.log('moviesdb is connected');
   } catch (err) {
     console.log(err.message);
